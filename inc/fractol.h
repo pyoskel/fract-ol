e926f2bd1445c13a12690486c0018a71e8b9c309
pabartoc <pabartoc@student.42heilbronn.de>
2026-07-20 02:04:09 +0200

    updated headerfile with define and libraries

--- inc/fractol.h
+++ inc/fractol.h
@@ -6,7 +6,17 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 01:59:58 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/07/11 01:59:59 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/07/20 01:49:50 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+/* --- Standard Libraries --- */
+# include <stdlib.h> // for malloc and free
+# include <unistd.h> // for write
+# include <limits.h> // for INT_MAX and INT_MIN
+// # include <stdio.h> // IMPORTANT: For printf during testing!
+
+#endif
