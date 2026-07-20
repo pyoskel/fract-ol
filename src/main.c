95fcea30a0f0a7d90099c5bfe71531ae1abd80cb
pabartoc <pabartoc@student.42heilbronn.de>
2026-07-20 02:06:49 +0200

    First thoughts for main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,16 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 02:10:13 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/07/11 02:10:14 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/07/20 02:05:39 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	main(int ac, char *av)
+{
+	if (mandelbrot set)
+	else if (julia set)
+	return (0);
+}
+
+// You must be able to create different Julia sets by passing different parameters to
+// the program
