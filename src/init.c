b05ab7c6d39e45a429fac0180c79ac4b2c165e7f
pyoskel <pasigermany@gmail.com>
2026-07-21 09:47:18 +0200

    build different Segmentation Fault

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 04:38:52 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/07/21 08:04:24 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/07/21 09:27:14 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,23 @@
 void	fractal_init(t_fractal *fractal)
 {
 	fractal->mlx = mlx_init();
+	if (fractal->mlx == NULL)
+		exit(EXIT_FAILURE);
 	fractal->window = mlx_new_window(fractal->mlx, 800, 800, fractal->name);
+	if (fractal->window == NULL)
+	{
+		mlx_destroy_display(fractal->mlx);
+		free(fractal->mlx);
+		exit(EXIT_FAILURE);
+	}
 	fractal->img = mlx_new_image(fractal->mlx, 800, 800);
+	if (fractal->img == NULL)
+	{
+		mlx_destroy_window(fractal->mlx, fractal->window);
+		mlx_destroy_display(fractal->mlx);
+		free(fractal->mlx);
+		exit(EXIT_FAILURE);
+	}
 	fractal->img_addr = mlx_get_data_addr(fractal->img, &fractal->bpp,
 			&fractal->line_length, &fractal->endian);
 }
