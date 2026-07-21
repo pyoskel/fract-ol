5e6452cf066b8626e6c4ccdce3b94e150ed2cc3a
pyoskel <pasigermany@gmail.com>
2026-07-21 09:06:54 +0200

    updated main. Include a print_help function and added the is_double_valid back

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 02:10:13 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/07/21 08:50:11 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/07/21 09:04:33 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,31 +21,34 @@
 // You must be able to create different Julia sets by passing
 // different parameters to the program
 
+static int	print_help(void)
+{
+	ft_putstr_fd("\nInvalid input!\n\n", 1);
+	ft_putstr_fd("Allowed parameters:\n", 1);
+	ft_putstr_fd("1. ./fractol mandelbrot\n", 1);
+	ft_putstr_fd("2. ./fractol julia <real> <imaginary>\n", 1);
+	ft_putstr_fd("Example: ./fractol julia -0.4 0.6\n\n", 1);
+	return (EXIT_FAILURE);
+}
+
 int	main(int ac, char **av)
 {
 	t_fractal	fractal;
 	
-	if ((ac == 2 && ft_strncmp(av[1], "mandelbrot", 11) == 0)
-		|| (ac == 4 && ft_strncmp(av[1], "julia", 6) == 0))
-	{
+	if (ac == 2 && ft_strncmp(av[1], "mandelbrot", 11) == 0)
 		fractal.name = av[1];
-		if (ac == 4)
-		{
-			fractal.julia_x = ft_atof(av[2]);
-			fractal.julia_y = ft_atof(av[3]);
-		}
-		printf("passed %s ✅\n", fractal.name);
-		printf("av[2] ✅ %f\n", fractal.julia_x);
-		printf("av[3] ✅ %f\n", fractal.julia_y);
-		fractal_init(&fractal);
-		mlx_loop(fractal.mlx);
+	else if (ac == 4 && ft_strncmp(av[1], "julia", 6) == 0)
+	{
+		if (!is_double_valid(av[2]) || !is_double_valid(av[3]))
+			return (print_help());
+		fractal.name = av[1];
+		fractal.julia_x = ft_atof(av[2]);
+		fractal.julia_y = ft_atof(av[3]);
 	}
 	else
-	{
-		printf("Error: Incorrect number!\n");
-		printf("Usage: ./fractol julia '-0.42' '0.42'\n");
-		return (EXIT_FAILURE);
-	}
+		return (print_help());
+	fractal_init(&fractal);
+	mlx_loop(fractal.mlx);
 	return (EXIT_SUCCESS);
 }
 
